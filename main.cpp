16ee6002ac6ff5679cd2e51e27de87eef10d8129
sepipi01 <sepipi01@wsc.edu>
2024-11-25 20:45:45 +0000

    updated the code and aligned spacing

--- main.cpp
+++ main.cpp
@@ -1,40 +1,52 @@
-// Intro
-#include <iostream>
-#include <vector>
+
 #include "MathTutor.h"
+#include <iostream>
 #include <string>
-#include <ctime>
-#include <limits>
+#include <cstdlib>
+#include <limits> 
+#include <vector> 
+#include <iomanip> 
 using namespace std;
 
-int main()
-{
+int main() {
+    int totalCorrect = 0; 
+    int totalIncorrect = 0; 
+    int mathLevel = 1; 
+    int currentRange = 10; 
+    vector<vector<int> > questions; 
+    string userName, userInput; 
+    bool playAgain = true;
 
-    int totalCorrect =0;
-    int totalIncorrect =0;
-    int mathLevel = 1;
-    int currentRange = 10;
-    vector<vector<int>> questions;
-    string userName = "?";
-    string userInput = "?";
-    bool playAgain = false;
-    srand(time(NULL));
+
+    srand(time(0));
+
+
     DisplayGameIntro();
     userName = GetUserName();
 
-    while (playAgain)
-    {
+    // Shows if the user wants to play again
+    while (playAgain) {
+        // Generate a random math question
         vector<int> question = GenerateRandomQuestion(mathLevel, currentRange);
-        GiveThreeAttempts(userName, question, totalIncorrect, totalCorrect);
+
+        // Allow the user to attempt the question
+        GiveThreeAttempts(userName, question, totalCorrect, totalIncorrect);
+
+        // Record the question for the summary
         questions.push_back(question);
 
-        CheckForLevelChange(totalIncorrect, totalCorrect, mathLevel, currentRange);
+        // Check for level changes
+        CheckForLevelChange(totalCorrect, totalIncorrect, mathLevel, currentRange);
+
+        // Ask the user if they want to play again
         userInput = AskToPlayAgain(userName);
-        playAgain = (userInput == "y"|| userInput == "yes");
+        playAgain = (userInput == "y" || userInput == "yes");
     }
+
+    // Shows the summary of the questions in a proper display
     DisplaySummaryReport(questions);
 
-    cout<< "Check back nex time" << endl;
+    // Displaying to the User that the Program is Finished and that it will be back.
+    cout << "Check back soon for the next version. Goodbye!" << endl;
     return 0;
-
 }
